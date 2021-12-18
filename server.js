d04751ce3bf30488dc220de419d195062f0f458f
pcancio <pcancio@gmail.com>
2021-12-18 11:47:21 -0500

    post

--- server.js
+++ server.js
@@ -5,8 +5,15 @@ const PORT = process.env.PORT || 3001;
 
 const bodyParser = require('body-parser');
 const path = require('path');
+const { application } = require('express');
+
+const notes = require('./db/db.json');
+console.log('notes', notes);
 
 app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 var options = {
     root: path.join(__dirname)
@@ -18,18 +25,33 @@ app.get('/notes', (req, res) => {
     })
 });
 
+app.get('/api/notes', (req, res) => {
+    console.log('in route');
+    res.json(notes);
+});
+
 app.get('*', (req, res) => {
     res.sendFile('/public/index.html', options, () => {
         console.log('index sent to browser');
     })
 });
 
-app.get('api/notes', (req, res) => {
-    console.log('in route');
-    fs.readFile('./db/db.json', (err, data) => {
-        if (err) console.error(err);
-        res.json(JSON.parse(data));
-    })
-});
+app.post('/api/notes', (req, res) => {
+        try {
+            console.log('request body', req.body);
+            const { title, text } = req.body
+            const newNote = { title, text }
+            console.log('newNote', newNote);
+            notes.push(newNote)
+            fs.writeFileSync('./db/db.json', JSON.stringify(notes))
+            res.status(200).json(newNote)
+        } catch (err) {
+            res.status(500).json(err)
+        }
+    }
 
-app.listen(PORT, () => console.log('listening on PORT ${PORT}'));
+)
+
+
+
+app.listen(PORT, () => console.log(`listening on PORT ${PORT}`));
