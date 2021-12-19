cfdb3ed4c03def9560bb528e8286819cd5ae57c7
pcancio <pcancio@gmail.com>
2021-12-19 16:10:07 -0500

    added comments

--- routes/html/index.js
+++ routes/html/index.js
@@ -1,14 +1,17 @@
 const path = require('path');
 const router = require('express').Router();
 
+// redirect main page to index.html
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
 
+// redirect note pages to notes.html
 router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../../public/notes.html'));
 });
 
+// redirect any other directory to index.html
 router.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
