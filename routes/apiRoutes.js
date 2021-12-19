3f0f05c629d861206773d9d3bd9db12ff2bc5085
pcancio <pcancio@gmail.com>
2021-12-19 13:51:42 -0500

    reinserted delete function

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -20,5 +20,15 @@ module.exports = function(app) {
         res.json(newNote)
     });
 
+    app.delete('/api/notes/:id', function(req, res) {
+        const notes = getJSON()
+        const id = parseInt(req.params.id)
+        console.log(id)
+        const filteredNotes = notes.filter(note => note.id !== id)
+        console.log(filteredNotes)
+        fs.writeFileSync('./db/db.json', JSON.stringify(filteredNotes), 'UTF-8')
+        res.sendStatus(200)
+    })
 
-}
+
+};
