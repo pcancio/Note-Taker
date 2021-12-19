8e9053275f5add79b6c705d18fea7976265c034b
pcancio <pcancio@gmail.com>
2021-12-19 14:18:27 -0500

    rewriting original with edits

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const notes = require('./db/db.json');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -6,7 +10,30 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-require('./routes/apiRoutes')(app);
-require('./routes/htmlRoutes')(app);
+function getJSON() {
+    return JSON.parse(fs.readFileSync('./db/db.json', 'UTF-8'))
+};
+// get commands
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+    return res.JSON(getJSON());
+});
+
+// post command
+app.post('/api/notes', (req, res) => {
+    const notesArray = getJSON();
+    let newNote = req.body;
+    newNote.id = notes.length - 1;
+    notesArray.push(newNote);
+    fs.writeFileSync('./db/db.json', JSON.stringify(notesArray), 'UTF-8')
+    res.json(newNote)
+});
 
 app.listen(PORT, () => console.log(`listening on PORT ${PORT}`));
